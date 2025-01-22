fe130e073ec38fb02d4a7b1bf994b01f59be2a0b
milfDestroller <adrianegeatorres@gmail.com>
2025-01-22 12:05:33 +0100

    a

---
+++ crearuser.js
@@ -0,0 +1,33 @@
+const list = ["rompe","revienta","encofrador","destruye","asesino","milf","carade"
+    ,"pringao","come","xxXThe_","el_mas","lolete","minecraftiano","vegeton","putilla",
+    "vegano","negro"
+]
+const list2 = ["madres","abuelo","menchu","maricon","ventana","vegeta","segarro","pinpin",
+    "cajetudo","pete","pelotudo","elmeho","venteamicasaajugaralawii","folagor","pokemaniaco",
+    "putero","torrente","cerveza","600pesetasdewisky"
+]
+const nombreuser = ""
+
+function randomFromArray(arr){
+    const num = Math.random() * arr.length
+    const numint = Math.floor(num)
+    return arr[numint]
+}
+function randomFromArray1(arr1){
+    const num1 = Math.random() * arr1.length
+    const numint1 = Math.floor(num1)
+    return arr1[numint1]
+}
+function randomNumber(){
+    const num2 = Math.random() * 9999 
+    const numint2 = Math.floor
+    
+}
+
+
+const elemento = randomFromArray(list)
+const elemento1 = randomFromArray1(list2)
+console.log(elemento)
+
+console.log(randomFromArray(["active", "inactive"]))
+console.log (elemento + "_" + elemento1)
--- server.js
+++ server.js
@@ -2,12 +2,33 @@ const express = require("express");
 
 const app = express();
 
-function HolaMundo(request, response){
-    console.log("hola mundo") // esto pa el servidor 
-    response.send("hola mundoo") //esto se devuelve al cliente
+const users = [
+{
+    "username": "admin",
+    "password": "admin",
+    "email": "admin@gmail.com",
+    "status": "active",
+},
+{
+    "username": "user1",
+    "password": "user2",
+    "email": "user2@gmail.com",
+    "status": "active",
+}
+]
+
+function getUsers(request,response){
+    response.json(users)
 }
 
-app.get("/", HolaMundo)
+app.get("/", (req, res) =>{
+    res.send
+})
+app.get("/users", getUsers)
+app.get("/users", (req,res) => {
+    res.json(users)
+})
+
 
 app.listen(3000, () =>{
     console.log("el servidor esta funcionando en el puerto 3000")
